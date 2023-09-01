db9c6eba3b69e393b7e1755e16fb966d17120f2f
unknown <lalwadiyashubham123@gmail.com>
2023-09-01 12:06:50 +0530

    main branch work done

---
+++ palindrom.js
@@ -0,0 +1,15 @@
+let x=1001;
+let str=x+"";
+let newstr="";
+
+for(let i= ;i>=0;i--)
+{
+}
+
+if(newstr==str){
+
+console.log("it is palindrome");
+}
+else{
+console.log("not a palindrome");
+}
---
+++ prime.js
@@ -0,0 +1,14 @@
+let num=20;
+let count=0;
+for(let i=0;i<=num;i++)
+{
+   if(
+}
+
+if(){
+
+console.log("Prime");
+}
+else{
+console.log("Not Prime");
+}
